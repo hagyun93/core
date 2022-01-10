202a5ea6dfe5af6d2904ecfe1107ec3dd2ccb48c
hagyun <imkimhagyun@naver.com>
2022-01-10 22:34:54 +0900

    Develop a new discount policy

---
+++ src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,35 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+
+    DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP should get 10% disconut")
+    void vip_o() {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("Discounts should not be applied unless you are a VIP.")
+    void vip_x() {
+        // given
+        Member member = new Member(2L, "memberBASIC", Grade.BASIC);
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}
